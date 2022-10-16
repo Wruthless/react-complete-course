c03b98b3c60995afe74e22603e945f85ad7dfc07
Wes Gonzales <wesgonzales@Wess-MacBook-Air.local>
2022-10-16 11:02:39 -0400

    State and hooks

--- my-app/src/components/Expenses/ExpenseItem.js
+++ my-app/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,26 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(
+        props.title
+    );
+
+    const clickHandler = () => {
+        setTitle('Updated');
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__descripton">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
             </div>
             <div className="expense-item__price">${props.amount}</div>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
